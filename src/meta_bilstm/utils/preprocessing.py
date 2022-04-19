b32806461fd997518b3c63cf4565289e1366d02c
Emanuel Huber da SIlva <ehs@cesar.org.br>
2022-04-18 23:15:04 -0300

    Lost 2h on this |

--- src/meta_bilstm/utils/preprocessing.py
+++ src/meta_bilstm/utils/preprocessing.py
@@ -1,9 +1,8 @@
-from sklearn.model_selection import train_test_split
 import conllu
+from sklearn.model_selection import train_test_split
 
 
 class Preprocessor:
-
     def __init__(self, test_size=0.25):
         self.test_size = test_size
 
@@ -18,9 +17,9 @@ class Preprocessor:
         for sentence in train_sentences:
             cur_sent = []
             for word in sentence:
-                preprocessed_word = self.preprocess_text(word['form'])
+                preprocessed_word = self.preprocess_text(word["form"])
                 chars = chars.union(set(preprocessed_word))
-                cur_sent.append((preprocessed_word, tag_to_idx[word['upos']]))
+                cur_sent.append((preprocessed_word, tag_to_idx[word["upos"]]))
             train_list_dataset.append(cur_sent)
 
         test_list_dataset = []
@@ -28,19 +27,19 @@ class Preprocessor:
         for sentence in test_sentences:
             cur_sent = []
             for word in sentence:
-                preprocessed_word = self.preprocess_text(word['form'])
+                preprocessed_word = self.preprocess_text(word["form"])
                 chars = chars.union(set(preprocessed_word))
-                cur_sent.append((preprocessed_word, tag_to_idx[word['upos']]))
+                cur_sent.append((preprocessed_word, tag_to_idx[word["upos"]]))
             test_list_dataset.append(cur_sent)
 
         char_to_idx = {char: idx + 3 for idx, char in enumerate(chars)}
         idx_to_char = {idx + 3: char for idx, char in enumerate(chars)}
 
-        char_to_idx['<pad>'] = 0
-        char_to_idx['|'] = 1
+        char_to_idx["<pad>"] = 0
+        char_to_idx["☕"] = 1
         char_to_idx["unknown"] = 2
-        idx_to_char[0] = '<pad>'
-        idx_to_char[1] = '|'
+        idx_to_char[0] = "<pad>"
+        idx_to_char[1] = "☕"
         idx_to_char[2] = "unknown"
 
         if test_sentences is None:
@@ -54,10 +53,7 @@ class Preprocessor:
                     "tag_to_idx": tag_to_idx,
                     "idx_to_tag": idx_to_tag,
                 },
-                "chars": {
-                    "char_to_idx": char_to_idx,
-                    "idx_to_char": idx_to_char
-                }
+                "chars": {"char_to_idx": char_to_idx, "idx_to_char": idx_to_char},
             }
         else:
             return {
@@ -69,16 +65,13 @@ class Preprocessor:
                     "tag_to_idx": tag_to_idx,
                     "idx_to_tag": idx_to_tag,
                 },
-                "chars": {
-                    "char_to_idx": char_to_idx,
-                    "idx_to_char": idx_to_char
-                }
+                "chars": {"char_to_idx": char_to_idx, "idx_to_char": idx_to_char},
             }
 
     def get_train_test_list_datasets(self, train_path, test_path):
-        with open(train_path, 'r') as f:
+        with open(train_path, "r") as f:
             train_sentences = conllu.parse(f.read())
-        with open(test_path, 'r') as f:
+        with open(test_path, "r") as f:
             test_sentences = conllu.parse(f.read())
         return self.convert_to_dataset(train_sentences, test_sentences)
 
@@ -87,7 +80,7 @@ class Preprocessor:
         tags = set()
         for sentence in sentences:
             for word in sentence:
-                tags.add(word['upos'])
+                tags.add(word["upos"])
         return tags
 
     @staticmethod
